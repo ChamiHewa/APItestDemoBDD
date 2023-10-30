303a454a747ae57d3b459e52783dbdf6587a6d27
ChamiHewa <chewawitharana+1@gmail.com>
2023-10-31 01:08:18 +0800

    Initial commit

---
+++ src/test/java/stepDefinitions/getCar.java
@@ -0,0 +1,40 @@
+package stepDefinitions;
+
+import io.restassured.RestAssured;
+import io.restassured.mapper.ObjectMapper;
+import io.restassured.response.Response;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class getCar {
+
+    @DataProvider(name = "cars")
+    public static Object[][] cars() {
+        return new Object[][] {{"Saloon", 200,"HTTP/1.1 200 OK"},{"SUV", 200,"HTTP/1.1 200 OK"},{"Sedan", 404,"HTTP/1.1 404 Not Found"}};
+    }
+
+    @Test(dataProvider = "cars")
+    public void VerifyCarTypeAgainstStatus(String type, int code, String statusLine) {
+
+        // Given
+        Response response = RestAssured.given()
+                .baseUri("http://localhost:54356/")
+                .when()
+                .get("api/Cars/" + type)
+                .then()
+                .statusCode(code)
+                .statusLine(statusLine)
+                .log().all()
+                .extract().response();
+
+        // Check if the status code is 200
+        if (response.getStatusCode() == 200) {
+            // To verify booking count
+            int responseCount = response.jsonPath().getList("cars").size();
+            System.out.println("Responses count: " + responseCount);
+        } else {
+            System.out.println("Status code is not 200.");
+        }
+    }
+}
